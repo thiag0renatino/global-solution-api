2ef3ac6c6b20f0dd8379bd6593f9edc54ee8016f
Thiago <thiagorenatino@gmail.com>
2025-05-29 13:00:06 -0300

    primeiro commit

---
+++ src/main/java/com/fiap/global_solution_api/GlobalSolutionApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.global_solution_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSolutionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalSolutionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/global_solution_api/GlobalSolutionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.global_solution_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSolutionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
